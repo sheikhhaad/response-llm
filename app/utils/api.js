cec386ab694c9e820dde7331e93a3e031164bdc4
sheikhhaad <sheikhhaad6@gmail.com>
2026-06-01 14:04:15 +0500

    ..

--- app/utils/api.js
+++ app/utils/api.js
@@ -1,7 +1,6 @@
 import axios from "axios";
 
-const API_URL =
-  "https://cfab-backend-c4oyaozbp-syed-haris-projects-930823a6.vercel.app";
+const API_URL = "https://cfab-backend.vercel.app";
 
 const api = axios.create({
   baseURL: `${API_URL}/api`,
